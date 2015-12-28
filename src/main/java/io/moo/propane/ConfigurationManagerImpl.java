07beb1d1191d5f9db85d8c031db32a0a77376304
Erhan Bagdemir <erhan.bagdemir@googlemail.com>
2015-12-28 14:41:01 +0100

    refactored superfluous else block

--- src/main/java/io/moo/propane/ConfigurationManagerImpl.java
+++ src/main/java/io/moo/propane/ConfigurationManagerImpl.java
@@ -1,18 +1,18 @@
 /**
  * The MIT License (MIT)
- * <p>
+ * <p/>
  * Copyright (c) 2015 moo.io , Erhan Bagdemir
- * <p>
+ * <p/>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * <p>
+ * <p/>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * <p>
+ * <p/>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,17 +27,15 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
-import io.moo.propane.annotation.Source;
-import io.moo.propane.providers.ConfigurationProvider;
-import io.moo.propane.providers.FileBackedConfigurationProviderImpl;
-import io.moo.propane.sources.ClasspathFileConfigurationSource;
-import io.moo.propane.sources.ConfigurationSource;
-import io.moo.propane.sources.PropertiesFileConfigurationSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import io.moo.propane.annotation.Configuration;
+import io.moo.propane.annotation.Source;
 import io.moo.propane.exception.InvalidConfigurationEntityException;
+import io.moo.propane.providers.ConfigurationProvider;
+import io.moo.propane.providers.FileBackedConfigurationProviderImpl;
+import io.moo.propane.sources.ConfigurationSource;
 
 /**
  * @author bagdemir
@@ -49,15 +47,15 @@ public class ConfigurationManagerImpl implements ConfigurationManager {
 
   private final Map<Class<?>, ConfigurationProvider> cache = new ConcurrentHashMap<>();
 
+
   @Override
   public <T> boolean register(final Class<T> clazz) {
     if (cache.containsKey(clazz)) {
       LOG.info("{} has already been registered.", clazz);
       return false;
-    } else {
-      validateConfigurationEntity(clazz);
-      registerConfigurationProvider(clazz);
     }
+    validateConfigurationEntity(clazz);
+    registerConfigurationProvider(clazz);
     return true;
   }
 
@@ -69,7 +67,6 @@ public class ConfigurationManagerImpl implements ConfigurationManager {
 
     cache.put(clazz, new FileBackedConfigurationProviderImpl<>(clazz, configSource, 60 /*TODO make configurable*/));
   }
-
 
 
   @Override
