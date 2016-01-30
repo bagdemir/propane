c57d48f8961e1c645d02bd6a80cd41e147994fd5
Bagdemir <erhan.bagdemir@googlemail.com>
2016-01-31 00:50:48 +0100

    classes renamed

--- src/main/java/io/moo/propane/data/ContextInfo.java
+++ src/main/java/io/moo/propane/data/ContextInfo.java
@@ -1,7 +1,29 @@
 package io.moo.propane.data;
 
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+
+import java.util.*;
+
 /**
  * Created by bagdemir on 30/01/16.
  */
 public class ContextInfo {
+  enum Regions {UE1, UW1, UW2, EW1, EC1, APS1, APN1, APS2, APN2, SAE1, US, EU, AP, SA}
+
+  private final List<String> contextIds = Collections.synchronizedList(Lists.newArrayList());
+
+  public void put(final String... contexts) {
+    Preconditions.checkNotNull(contexts, "contexts may not be null.");
+    contextIds.addAll(Arrays.asList(contexts));
+  }
+
+  public void put(final String context) {
+    Preconditions.checkNotNull(context, "context may not be null.");
+    contextIds.add(context);
+  }
+
+  public List<String> getContexts() {
+    return contextIds;
+  }
 }
