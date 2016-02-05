6912036482601b6f32fc6611b02068865de1bc25
Erhan Bagdemir <bagdemir@adobe.com>
2016-02-05 20:06:42 +0100

    fix for configuration merge

--- src/main/java/io/moo/propane/annotation/processor/ConfigurationAnnotationProcessorImpl.java
+++ src/main/java/io/moo/propane/annotation/processor/ConfigurationAnnotationProcessorImpl.java
@@ -1,18 +1,18 @@
 /**
  * The MIT License (MIT)
- * <p>
+ * <p/>
  * Copyright (c) 2015 moo.io , Erhan Bagdemir
- * <p>
+ * <p/>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * <p>
+ * <p/>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * <p>
+ * <p/>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,20 +23,6 @@
  */
 package io.moo.propane.annotation.processor;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import io.moo.propane.annotation.Configuration;
 import io.moo.propane.annotation.KeyValue;
 import io.moo.propane.annotation.Source;
@@ -45,6 +31,12 @@ import io.moo.propane.data.ContextInfo;
 import io.moo.propane.exception.InvalidConfigurationEntityException;
 import io.moo.propane.extractors.TokenExtractor;
 import io.moo.propane.sources.ConfigData;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.lang.reflect.Field;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Process the configuration annotations provided in configuration entities.
@@ -54,185 +46,196 @@ import io.moo.propane.sources.ConfigData;
  * @since 1.0
  */
 public class ConfigurationAnnotationProcessorImpl implements AnnotationProcessor {
-  private static final Logger LOG = LogManager.getLogger();
+    private static final Logger LOG = LogManager.getLogger();
 
-
-  private boolean isWithinContext(final Optional<ContextInfo> contextInfo, final ConfigurationEntity entity) {
-    if (!contextInfo.isPresent() || contextInfo.get().getContexts().isEmpty()) {
-      return true;
+    @Override
+    public <T> T createEntity(final Class<T> clazz, final ConfigData data) {
+        return createEntity(clazz, data, Optional.empty());
     }
-    return entity.getContextIds().containsAll(contextInfo.get().getContexts());
-  }
 
 
-  @Override
-  public <T> T createEntity(final Class<T> clazz, final ConfigData data) {
-    return createEntity(clazz, data, Optional.empty());
-  }
+    public <T> T createEntity(final Class<T> clazz, final ConfigData configData, final Optional<ContextInfo> contextInfo) {
+        final Source source = clazz.getDeclaredAnnotation(Source.class);
+        final TokenExtractor idExtractor = getComponentIdExtractorInstance(source);
+        final TokenExtractor contextExtractor = getComponentContextExtractorInstance(source);
+        final List<ConfigurationEntity> entities = configData.getPropsMap().entrySet().stream().
+                map(entity -> AnnotationProcessor.newEntity(idExtractor, contextExtractor, configData, entity)).
+                collect(Collectors.toList());
+
+        final Configuration configurationAnnotation = clazz.getDeclaredAnnotation(Configuration.class);
+        if (configurationAnnotation != null) {
+            final T instance = newEntityInstance(clazz);
+            processFields(filterEntities(entities, contextInfo), configurationAnnotation.componentId(), clazz.getDeclaredFields(), instance);
+            return instance;
+        } else
+            throw new InvalidConfigurationEntityException("@Configuration annotation is missing.");
+    }
 
 
-  public <T> T createEntity(final Class<T> clazz, final ConfigData configData, final Optional<ContextInfo> contextInfo) {
-    final Source source = clazz.getDeclaredAnnotation(Source.class);
-    final TokenExtractor idExtractor = getComponentIdExtractorInstance(source);
-    final TokenExtractor contextExtractor = getComponentContextExtractorInstance(source);
-    final List<ConfigurationEntity> entities = configData.getPropsMap().entrySet().stream().
-            map(entity -> AnnotationProcessor.newEntity(idExtractor, contextExtractor, configData, entity)).
-            collect(Collectors.toList());
+    private List<ConfigurationEntity> filterEntities(final List<ConfigurationEntity> entities, final Optional<ContextInfo> contextInfo) {
+        // Sort the entities according to their number of context ids.
+        Collections.sort(entities, (first, second) -> first.getContextIds().size() - second.getContextIds().size());
 
-    final Configuration configurationAnnotation = clazz.getDeclaredAnnotation(Configuration.class);
-    if (configurationAnnotation != null) {
-      final T instance = newEntityInstance(clazz);
-      filterEntities(entities, contextInfo);
-      processFields(entities, configurationAnnotation.componentId(), clazz.getDeclaredFields(), instance);
-      return instance;
-    } else
-      throw new InvalidConfigurationEntityException("@Configuration annotation is missing.");
-  }
+        final List<ConfigurationEntity> result = new ArrayList<>();
+        final List<ConfigurationEntity> overriddenElements = new ArrayList<>();
+        final Iterator<ConfigurationEntity> iterator = entities.iterator();
 
+        int current = 0;
 
-  private List<ConfigurationEntity> filterEntities(final List<ConfigurationEntity> entities, final Optional<ContextInfo> contextInfo) {
-    Collections.sort(entities, (first, second) -> first.getContextIds().size() - second.getContextIds().size());
-    final List<ConfigurationEntity> result = new ArrayList<>();
+        while (iterator.hasNext()) {
+            ConfigurationEntity next = iterator.next();
+            Collection<String> contextIds = next.getContextIds();
+            // If context info is provided, then look for suitable configs.
+            if (contextInfo.isPresent()) {
+                final ListIterator<ConfigurationEntity>
+                        configurationEntityListIterator = entities.listIterator(current);
 
-    final Iterator<ConfigurationEntity> iterator = entities.iterator();
-    int current = 0;
-    while (iterator.hasNext()) {
+                if (!result.contains(next) && !overriddenElements.contains
+                        (next)) {
+                    result.add(next);
+                }
 
-      final ConfigurationEntity next = iterator.next();
-      final Collection<String> contextIds = next.getContextIds();
-      final ListIterator<ConfigurationEntity> configurationEntityListIterator = entities.listIterator(current);
-      while (configurationEntityListIterator.hasNext()) {
-        final ConfigurationEntity entity = configurationEntityListIterator.next();
-        if (AnnotationProcessor.getPropertyNameFromFQPropertyName(next.getPropertyName()).equals(AnnotationProcessor.getPropertyNameFromFQPropertyName(entity.getPropertyName())) &&
-                entity.getContextIds().size() > contextIds.size() &&
-                contextInfo.get().getContexts().containsAll(entity.getContextIds())) {
+                while (configurationEntityListIterator.hasNext()) {
+                    ConfigurationEntity entity = configurationEntityListIterator.next();
+                    if (AnnotationProcessor.getPropertyNameFromFQPropertyName(next.getPropertyName()).
+                            equals(AnnotationProcessor.getPropertyNameFromFQPropertyName(entity.getPropertyName())) &&
+                            entity.getContextIds().size() > contextIds.size()) {
 
-        } else {
-          if (!result.contains(next)) result.add(next);
+                        if (contextInfo.get().getContexts().containsAll(entity
+                                .getContextIds())) {
+                            if (result.contains(next)) result.remove(next);
+                        } else {
+                            overriddenElements.add(entity);
+                        }
+                    }
+                }
+                current++;
+            } else {
+                // if context info is not provided, then look for global
+                // configs.
+                if (next.getContextIds().isEmpty() && !result.contains(next)) {
+                    result.add(next);
+                }
+            }
         }
-      }
-      current++;
+
+        overriddenElements.clear();
+
+        return result;
     }
 
-    return result;
-  }
 
+    private TokenExtractor getComponentIdExtractorInstance(final Source source) {
 
-  private TokenExtractor getComponentIdExtractorInstance(final Source source) {
-
-    try {
-      if (source == null) {
-        throw new InvalidConfigurationEntityException("@Source annotation is missing.");
-      } else {
-        return source.componentIdExtractor().newInstance();
-      }
+        try {
+            if (source == null) {
+                throw new InvalidConfigurationEntityException("@Source annotation is missing.");
+            } else {
+                return source.componentIdExtractor().newInstance();
+            }
+        } catch (InstantiationException | IllegalAccessException e) {
+            LOG.error(e);
+            throw new InvalidConfigurationEntityException(e.getMessage(), e);
+        }
     }
-    catch (InstantiationException | IllegalAccessException e) {
-      LOG.error(e);
-      throw new InvalidConfigurationEntityException(e.getMessage(), e);
+
+
+    private TokenExtractor getComponentContextExtractorInstance(final Source source) {
+
+        try {
+            if (source == null) {
+                throw new InvalidConfigurationEntityException("@Source annotation is missing.");
+            } else {
+                return source.contextExtractor().newInstance();
+            }
+        } catch (InstantiationException | IllegalAccessException e) {
+            LOG.error(e);
+            throw new InvalidConfigurationEntityException(e.getMessage(), e);
+        }
     }
-  }
 
 
-  private TokenExtractor getComponentContextExtractorInstance(final Source source) {
+    private <T> void processFields(final Collection<ConfigurationEntity> entities,
+                                   final String componentId, final Field[] fields, final T instance) {
 
-    try {
-      if (source == null) {
-        throw new InvalidConfigurationEntityException("@Source annotation is missing.");
-      } else {
-        return source.contextExtractor().newInstance();
-      }
+        Arrays.stream(fields).
+                filter(field -> null != field.getAnnotation(KeyValue.class)).
+                forEach(field -> entities.stream().
+                        filter(entity -> entity.getComponentId().equals(componentId)).
+                        forEach(entity -> performEntityProcessing(field.getAnnotation(KeyValue.class),
+                                entity, field, instance)));
     }
-    catch (InstantiationException | IllegalAccessException e) {
-      LOG.error(e);
-      throw new InvalidConfigurationEntityException(e.getMessage(), e);
+
+
+    private <T> void performEntityProcessing(final KeyValue annotation, final ConfigurationEntity configurationEntity,
+                                             final Field field, T instance) {
+
+        if (annotation.name().equals(configurationEntity.getPropertyName())) {
+            Object propertyValue = configurationEntity.getPropertyValue();
+            if (isWrapper(field.getType()) || field.getType().isPrimitive()) {
+                performPrimitiveAssignment(instance, field, propertyValue);
+            } else if (field.getType().isAssignableFrom(propertyValue.getClass())) {
+                performAssignment(instance, field, propertyValue);
+            }
+        }
     }
-  }
 
 
-  private <T> void processFields(final Collection<ConfigurationEntity> entities,
-          final String componentId, final Field[] fields, final T instance) {
-
-    Arrays.stream(fields).
-            filter(field -> null != field.getAnnotation(KeyValue.class)).
-            forEach(field -> entities.stream().
-                    filter(entity -> entity.getComponentId().equals(componentId)).
-                    forEach(entity -> performEntityProcessing(field.getAnnotation(KeyValue.class),
-                            entity, field, instance)));
-  }
-
-
-  private <T> void performEntityProcessing(final KeyValue annotation, final ConfigurationEntity configurationEntity,
-          final Field field, T instance) {
-
-    if (annotation.name().equals(configurationEntity.getPropertyName())) {
-      Object propertyValue = configurationEntity.getPropertyValue();
-      if (isWrapper(field.getType()) || field.getType().isPrimitive()) {
-        performPrimitiveAssignment(instance, field, propertyValue);
-      } else if (field.getType().isAssignableFrom(propertyValue.getClass())) {
-        performAssignment(instance, field, propertyValue);
-      }
+    private boolean isWrapper(final Class<?> type) {
+        return type.equals(Integer.class) || type.equals(Long.class) ||
+                type.equals(Double.class) ||
+                type.equals(Float.class) ||
+                type.equals(Short.class) ||
+                type.equals(Byte.class) ||
+                type.equals(Boolean.class) ||
+                type.equals(Character.class);
     }
-  }
 
 
-  private boolean isWrapper(final Class<?> type) {
-    return type.equals(Integer.class) || type.equals(Long.class) ||
-            type.equals(Double.class) ||
-            type.equals(Float.class) ||
-            type.equals(Short.class) ||
-            type.equals(Byte.class) ||
-            type.equals(Boolean.class) ||
-            type.equals(Character.class);
-  }
-
-
-  private <T> void performPrimitiveAssignment(final T instance,
-          final Field field,
-          final Object propertyValue) {
-    final Class<?> fieldType = field.getType();
-    if (fieldType.equals(int.class) || fieldType.equals(Integer.class)) {
-      performAssignment(instance, field, Integer.valueOf((String) propertyValue));
-    } else if (fieldType.equals(long.class) || fieldType.equals(Long.class)) {
-      performAssignment(instance, field, Long.valueOf((String) propertyValue));
-    } else if (fieldType.equals(double.class) || fieldType.equals(Double.class)) {
-      performAssignment(instance, field, Double.valueOf((String) propertyValue));
-    } else if (fieldType.equals(float.class) || fieldType.equals(Float.class)) {
-      performAssignment(instance, field, Float.valueOf((String) propertyValue));
-    } else if (fieldType.equals(short.class) || fieldType.equals(Short.class)) {
-      performAssignment(instance, field, Short.valueOf((String) propertyValue));
-    } else if (fieldType.equals(byte.class) || fieldType.equals(Byte.class)) {
-      performAssignment(instance, field, Byte.valueOf((String) propertyValue));
-    } else if (fieldType.equals(char.class) || fieldType.equals(Character.class)) {
-      performAssignment(instance, field, Byte.valueOf((String) propertyValue));
-    } else if (fieldType.equals(boolean.class) || fieldType.equals(Boolean.class)) {
-      performAssignment(instance, field, Boolean.valueOf((String) propertyValue));
+    private <T> void performPrimitiveAssignment(final T instance,
+                                                final Field field,
+                                                final Object propertyValue) {
+        final Class<?> fieldType = field.getType();
+        if (fieldType.equals(int.class) || fieldType.equals(Integer.class)) {
+            performAssignment(instance, field, Integer.valueOf((String) propertyValue));
+        } else if (fieldType.equals(long.class) || fieldType.equals(Long.class)) {
+            performAssignment(instance, field, Long.valueOf((String) propertyValue));
+        } else if (fieldType.equals(double.class) || fieldType.equals(Double.class)) {
+            performAssignment(instance, field, Double.valueOf((String) propertyValue));
+        } else if (fieldType.equals(float.class) || fieldType.equals(Float.class)) {
+            performAssignment(instance, field, Float.valueOf((String) propertyValue));
+        } else if (fieldType.equals(short.class) || fieldType.equals(Short.class)) {
+            performAssignment(instance, field, Short.valueOf((String) propertyValue));
+        } else if (fieldType.equals(byte.class) || fieldType.equals(Byte.class)) {
+            performAssignment(instance, field, Byte.valueOf((String) propertyValue));
+        } else if (fieldType.equals(char.class) || fieldType.equals(Character.class)) {
+            performAssignment(instance, field, Byte.valueOf((String) propertyValue));
+        } else if (fieldType.equals(boolean.class) || fieldType.equals(Boolean.class)) {
+            performAssignment(instance, field, Boolean.valueOf((String) propertyValue));
+        }
     }
-  }
 
 
-  private <T> void performAssignment(final T instance,
-          final Field field,
-          final Object propertyValue) {
-    try {
-      field.setAccessible(true);
-      field.set(instance, propertyValue);
+    private <T> void performAssignment(final T instance,
+                                       final Field field,
+                                       final Object propertyValue) {
+        try {
+            field.setAccessible(true);
+            field.set(instance, propertyValue);
+        } catch (IllegalAccessException e) {
+            LOG.error(e);
+        } finally {
+            field.setAccessible(false);
+        }
     }
-    catch (IllegalAccessException e) {
-      LOG.error(e);
-    } finally {
-      field.setAccessible(false);
-    }
-  }
 
 
-  private <T> T newEntityInstance(final Class<T> clazz) {
-    try {
-      return clazz.newInstance();
+    private <T> T newEntityInstance(final Class<T> clazz) {
+        try {
+            return clazz.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            LOG.error(e);
+        }
+        throw new AssertionError("Cannot createEntity a new instance of :" + clazz.getName());
     }
-    catch (InstantiationException | IllegalAccessException e) {
-      LOG.error(e);
-    }
-    throw new AssertionError("Cannot createEntity a new instance of :" + clazz.getName());
-  }
 }
--- src/test/java/io/moo/propane/ConfigurationManagerTest.java
+++ src/test/java/io/moo/propane/ConfigurationManagerTest.java
@@ -1,18 +1,18 @@
 /**
  * The MIT License (MIT)
- * <p>
+ * <p/>
  * Copyright (c) 2015 moo.io , Erhan Bagdemir
- * <p>
+ * <p/>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * <p>
+ * <p/>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * <p>
+ * <p/>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,18 +23,17 @@
  */
 package io.moo.propane;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
-
-import java.util.Optional;
-
+import io.moo.propane.annotation.TestConfigEntity;
+import io.moo.propane.data.ContextInfo;
+import io.moo.propane.exception.InvalidConfigurationEntityException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 
-import io.moo.propane.annotation.TestConfigEntity;
-import io.moo.propane.data.ContextInfo;
-import io.moo.propane.exception.InvalidConfigurationEntityException;
+import java.util.Optional;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Unit test for the configuration manager.
@@ -44,67 +43,87 @@ import io.moo.propane.exception.InvalidConfigurationEntityException;
  * @since 1.0
  */
 public class ConfigurationManagerTest {
-  private static final Logger LOG = LogManager.getLogger();
-  private static final String TEST_PROPS = "configurations/test.properties";
+    private static final Logger LOG = LogManager.getLogger();
+    private static final String TEST_PROPS = "configurations/test.properties";
 
 
-  @Test(expected = InvalidConfigurationEntityException.class)
-  public void testIsRegisteredTestUsingInvalidPropsEntity() {
-    final ConfigurationManager configurationManager = ConfigurationManager
-            .newManager(Optional.empty());
-    configurationManager.register(Object.class);
-  }
+    @Test(expected = InvalidConfigurationEntityException.class)
+    public void testIsRegisteredTestUsingInvalidPropsEntity() {
+        final ConfigurationManager configurationManager = ConfigurationManager
+                .newManager(Optional.empty());
+        configurationManager.register(Object.class);
+    }
 
 
-  @Test
-  public void testIsRegisteredTestUsingValidPropsEntityButAlreadyRegistered() {
-    final ConfigurationManager configurationManager = ConfigurationManager
-            .newManager(Optional.empty());
-    assertThat(configurationManager.register(TestConfigEntity.class), equalTo(true));
-    assertThat(configurationManager.register(TestConfigEntity.class), equalTo(false));
-  }
+    @Test
+    public void testIsRegisteredTestUsingValidPropsEntityButAlreadyRegistered() {
+        final ConfigurationManager configurationManager = ConfigurationManager
+                .newManager(Optional.empty());
+        assertThat(configurationManager.register(TestConfigEntity.class), equalTo(true));
+        assertThat(configurationManager.register(TestConfigEntity.class), equalTo(false));
+    }
 
 
-  @Test
-  public void testIsRegisteredTestUsingValidPropsEntity() {
-    final ConfigurationManager configurationManager = ConfigurationManager
-            .newManager(Optional.empty());
-    configurationManager.register(TestConfigEntity.class);
-    assertThat(configurationManager.isRegistered(TestConfigEntity.class), equalTo(true));
-  }
+    @Test
+    public void testIsRegisteredTestUsingValidPropsEntity() {
+        final ConfigurationManager configurationManager = ConfigurationManager
+                .newManager(Optional.empty());
+        configurationManager.register(TestConfigEntity.class);
+        assertThat(configurationManager.isRegistered(TestConfigEntity.class), equalTo(true));
+    }
 
 
-  @Test
-  public void testLoadFromClasspath() throws InterruptedException {
-    final ConfigurationManager configurationManager = ConfigurationManager
-            .newManager(Optional.empty());
-    configurationManager.register(TestConfigEntity.class);
+    @Test
+    public void testLoadFromClasspath() throws InterruptedException {
+        final ConfigurationManager configurationManager = ConfigurationManager
+                .newManager(Optional.empty());
+        configurationManager.register(TestConfigEntity.class);
 
-    Thread.sleep(100L);
-    final Optional<TestConfigEntity> configs = configurationManager.load(TestConfigEntity.class);
-    assertThat(configs.isPresent(), equalTo(true));
+        Thread.sleep(100L);
+        final Optional<TestConfigEntity> configs = configurationManager.load(TestConfigEntity.class);
+        assertThat(configs.isPresent(), equalTo(true));
 
-    final TestConfigEntity testPropsWithClasspathSource = configs.get();
-    assertThat(testPropsWithClasspathSource.getUrl(), equalTo("http://localhost/"));
-    assertThat(testPropsWithClasspathSource.getTimeout(), equalTo(1000L));
-    assertThat(testPropsWithClasspathSource.getCount(), equalTo(99));
-  }
+        final TestConfigEntity testPropsWithClasspathSource = configs.get();
+        assertThat(testPropsWithClasspathSource.getUrl(), equalTo("http://localhost/"));
+        assertThat(testPropsWithClasspathSource.getTimeout(), equalTo(1000L));
+        assertThat(testPropsWithClasspathSource.getCount(), equalTo(99));
+    }
 
-  @Test
-  public void testLoadFromClasspathWithContext() throws InterruptedException {
-    final ContextInfo contextInfo = ContextInfo.of("eu", "dev");
+    @Test
+    public void testLoadFromClasspathWithMultipleContext() throws
+            InterruptedException {
+        final ContextInfo contextInfo = ContextInfo.of("eu", "dev");
 
-    final ConfigurationManager configurationManager = ConfigurationManager
-            .newManager(Optional.of(contextInfo));
-    configurationManager.register(TestConfigEntity.class);
+        final ConfigurationManager configurationManager = ConfigurationManager
+                .newManager(Optional.of(contextInfo));
+        configurationManager.register(TestConfigEntity.class);
 
-    Thread.sleep(100L);
-    final Optional<TestConfigEntity> configs = configurationManager.load(TestConfigEntity.class, Optional.of(contextInfo));
-    assertThat(configs.isPresent(), equalTo(true));
+        Thread.sleep(100L);
+        final Optional<TestConfigEntity> configs = configurationManager.load(TestConfigEntity.class, Optional.of(contextInfo));
+        assertThat(configs.isPresent(), equalTo(true));
 
-    final TestConfigEntity testPropsWithClasspathSource = configs.get();
-    assertThat(testPropsWithClasspathSource.getUrl(), equalTo("http://localhost/"));
-    assertThat(testPropsWithClasspathSource.getTimeout(), equalTo(1001L));
-    assertThat(testPropsWithClasspathSource.getCount(), equalTo(99));
-  }
+        final TestConfigEntity testPropsWithClasspathSource = configs.get();
+        assertThat(testPropsWithClasspathSource.getUrl(), equalTo("http://localhost/"));
+        assertThat(testPropsWithClasspathSource.getTimeout(), equalTo(1002L));
+        assertThat(testPropsWithClasspathSource.getCount(), equalTo(99));
+    }
+
+    @Test
+    public void testLoadFromClasspathWithSingleContext() throws
+            InterruptedException {
+        final ContextInfo contextInfo = ContextInfo.of("eu");
+
+        final ConfigurationManager configurationManager = ConfigurationManager
+                .newManager(Optional.of(contextInfo));
+        configurationManager.register(TestConfigEntity.class);
+
+        Thread.sleep(100L);
+        final Optional<TestConfigEntity> configs = configurationManager.load(TestConfigEntity.class, Optional.of(contextInfo));
+        assertThat(configs.isPresent(), equalTo(true));
+
+        final TestConfigEntity testPropsWithClasspathSource = configs.get();
+        assertThat(testPropsWithClasspathSource.getUrl(), equalTo("http://localhost/"));
+        assertThat(testPropsWithClasspathSource.getTimeout(), equalTo(1001L));
+        assertThat(testPropsWithClasspathSource.getCount(), equalTo(99));
+    }
 }
