aad229d4075756c65d332e2cec38cfcea1c38adc
Erhan Bagdemir <erhan.bagdemir@googlemail.com>
2016-04-26 21:42:21 +0200

    style guide xml file for idea.

--- propane-core/src/main/java/io/moo/propane/sources/ClasspathFileConfigurationSource.java
+++ propane-core/src/main/java/io/moo/propane/sources/ClasspathFileConfigurationSource.java
@@ -1,29 +1,25 @@
 /**
- * The MIT License (MIT)
- * <p>
- * Copyright (c) 2015 moo.io , Erhan Bagdemir
- * <p>
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * <p>
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * <p>
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * The MIT License (MIT) <p> Copyright (c) 2015 moo.io , Erhan Bagdemir <p> Permission is hereby
+ * granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions: <p> The above copyright notice and this permission
+ * notice shall be included in all copies or substantial portions of the Software. <p> THE SOFTWARE
+ * IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+ * EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+ * THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package io.moo.propane.sources;
 
-import static io.moo.propane.annotation.processor.AnnotationUtils.getDefinedProperties;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -34,17 +30,11 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.TreeSet;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-
-import io.moo.propane.annotation.processor.AnnotationUtils;
 import io.moo.propane.data.ConfigurationEntity;
 import io.moo.propane.extractors.DefaultComponentIdExtractor;
 import io.moo.propane.extractors.TokenExtractor;
+
+import static io.moo.propane.annotation.processor.AnnotationUtils.getDefinedProperties;
 
 
 /**
@@ -56,75 +46,74 @@ import io.moo.propane.extractors.TokenExtractor;
  * @since 1.0
  */
 public class ClasspathFileConfigurationSource extends ConfigurationSource {
-  private static final Logger LOG = LogManager.getLogger();
+    private static final Logger LOG = LogManager.getLogger();
 
-  private final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-  private final TokenExtractor componentIdExtractor = new
-          DefaultComponentIdExtractor();
-
-
-  public ClasspathFileConfigurationSource(final String source,
-                                          final Class<?> entityType) {
-    super(source, entityType);
-  }
+    private final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+    private final TokenExtractor componentIdExtractor = new
+            DefaultComponentIdExtractor();
 
 
-  @Override
-  public Optional<ConfigData> read(final Class clazz) {
-
-    try (InputStreamReader reader = new InputStreamReader(getClass()
-            .getClassLoader().getResourceAsStream(source))) {
-
-      List<ConfigurationEntity> entities = new ArrayList<>();
-
-      parse(getSource(), mapper.readTree(reader), new ArrayList<>(),
-              entities, 0);
-
-      return Optional.of(new ConfigData(getSource(), entities));
-
+    public ClasspathFileConfigurationSource(final String source,
+                                            final Class<?> entityType) {
+        super(source, entityType);
     }
-    catch (IOException e) {
-      LOG.error(e);
-      return Optional.empty();
-    }
-  }
 
 
-  private void parse(
-          final String source,
-          final JsonNode jsonNode,
-          final List<String> contextIds,
-          final List<ConfigurationEntity> entities,
-          final int level) {
+    @Override
+    public Optional<ConfigData> read(final Class clazz) {
 
-    final Iterator<Map.Entry<String, JsonNode>> it = jsonNode.fields();
+        try (InputStreamReader reader = new InputStreamReader(getClass()
+                .getClassLoader().getResourceAsStream(source))) {
 
-    while (it.hasNext()) {
+            List<ConfigurationEntity> entities = new ArrayList<>();
 
-      final Map.Entry<String, JsonNode> next = it.next();
-      final List<String> definedProperties = getDefinedProperties(getEntityType());
+            parse(getSource(), mapper.readTree(reader), new ArrayList<>(),
+                    entities, 0);
 
-      if (!definedProperties.contains(next.getKey()) && next.getValue()
-              .isContainerNode()) {
-        if (level == 0) {
-          final List<String> ids = new ArrayList<>();
-          ids.add(next.getKey());
-          parse(source, next.getValue(), ids, entities, level + 1);
-        } else {
-          contextIds.add(next.getKey());
-          parse(source, next.getValue(), contextIds, entities, level);
+            return Optional.of(new ConfigData(getSource(), entities));
+
+        } catch (IOException e) {
+            LOG.error(e);
+            return Optional.empty();
         }
-
-      } else if (definedProperties.contains(next.getKey())) {
-        ConfigurationEntity configurationEntity = new ConfigurationEntity(componentIdExtractor.extract
-                (source).iterator().next(), new
-                TreeSet<>(contextIds), next
-                .getKey(),
-                next.getValue().asText());
-
-        LOG.info("Creating entity: {}", configurationEntity);
-        entities.add(configurationEntity);
-      }
     }
-  }
+
+
+    private void parse(
+            final String source,
+            final JsonNode jsonNode,
+            final List<String> contextIds,
+            final List<ConfigurationEntity> entities,
+            final int level) {
+
+        final Iterator<Map.Entry<String, JsonNode>> it = jsonNode.fields();
+
+        while (it.hasNext()) {
+
+            final Map.Entry<String, JsonNode> next = it.next();
+            final List<String> definedProperties = getDefinedProperties(getEntityType());
+
+            if (!definedProperties.contains(next.getKey()) && next.getValue()
+                    .isContainerNode()) {
+                if (level == 0) {
+                    final List<String> ids = new ArrayList<>();
+                    ids.add(next.getKey());
+                    parse(source, next.getValue(), ids, entities, level + 1);
+                } else {
+                    contextIds.add(next.getKey());
+                    parse(source, next.getValue(), contextIds, entities, level);
+                }
+
+            } else if (definedProperties.contains(next.getKey())) {
+                ConfigurationEntity configurationEntity = new ConfigurationEntity(componentIdExtractor.extract
+                        (source).iterator().next(), new
+                        TreeSet<>(contextIds), next
+                        .getKey(),
+                        next.getValue().asText());
+
+                LOG.info("Creating entity: {}", configurationEntity);
+                entities.add(configurationEntity);
+            }
+        }
+    }
 }
